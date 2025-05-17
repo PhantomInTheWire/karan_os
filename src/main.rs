2ac2aafa56f6ddfaf2ae42b564facf278ae8fb10
Karan <karanlokchandani@protonmail.com>
2025-05-18 01:44:52 +0530

    blog 3 done

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
 mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    print!("{}", "hello world");
+    println!("This is Karan {}", "I need time....");
     loop {}
 }
