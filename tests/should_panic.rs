58a45c3a9e84f0d5e134b54ec2021210841fc8d6
Karan <karanlokchandani@protonmail.com>
2025-05-18 19:52:27 +0530

    fmt

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use karanos::{exit_qemu, serial_print, serial_println, QemuExitCode};
+use karanos::{QemuExitCode, exit_qemu, serial_print, serial_println};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
