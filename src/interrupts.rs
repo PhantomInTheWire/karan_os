f048004681ed24f4453e75873ec1802ab76d0cad
Karan <karanlokchandani@protonmail.com>
2025-05-18 17:38:19 +0530

    fmt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,9 @@
+use crate::println;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::println;
 
 lazy_static! {
-static ref IDT: InterruptDescriptorTable = {
+    static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt
@@ -14,9 +14,7 @@ pub fn init_idt() {
     IDT.load();
 }
 
-extern "x86-interrupt" fn breakpoint_handler(
-    stack_frame: InterruptStackFrame)
-{
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use karanos::{QemuExitCode, exit_qemu, serial_print, serial_println};
+use karanos::{exit_qemu, serial_print, serial_println, QemuExitCode};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
